ebc2de2f0f5bca3b54e9852ffc08a972f6c9ad3a
viktor <kamiknol@gmail.com>
2025-06-05 22:21:41 +0300

    fixing

--- src/main/java/me/viktrl/VpnTgBot/service/TelegramBot.java
+++ src/main/java/me/viktrl/VpnTgBot/service/TelegramBot.java
@@ -289,6 +289,7 @@ public class TelegramBot extends TelegramLongPollingBot {
                             "\nКлюч для ВПН: " + user.getTokenKey() +
                             "\nПромокод: " + promocodes.getCode() +
                             "\nИспользовано трафика:\n" + new GsonBuilder().setPrettyPrinting().create().toJson(answerForMe) +
+                            "\nНе используют ключ:\n" + new GsonBuilder().setPrettyPrinting().create().toJson(answerForMe) +
                             "\nНеактивированные пользователи:\n" + new GsonBuilder().setPrettyPrinting().create().toJson(answerInactiveForMe);
 
                     sendMessage(chatId, showAdminAnswer);
